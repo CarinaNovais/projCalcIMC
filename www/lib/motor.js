2b9c5ef3d66ca81f75916ce46ba864bcd31f4aac
CarinaNovais <carinapereiraN@outlook.com>
2022-02-17 00:19:11 +0900

    retirando style do button/\(css\)/Adicionando mensagem informativa /\(html\)/Renomeando as fotos

--- www/lib/motor.js
+++ www/lib/motor.js
@@ -13,16 +13,33 @@
         if(imc < 18.5){
         result.innerHTML = imc;
         msg.innerHTML = "Abaixo do Peso";
-        document.getElementById('foto').src = "lib/img/magro.png";
+        document.getElementById('foto').src = "lib/img/abaixo.png";
            
         }else if(imc > 18.5 && imc < 24.9){
              result.innerHTML = imc;
               msg.innerHTML = "Parabéns!! Seu peso é ideal";
-            document.getElementById('foto').src = "lib/img/normal.png";
-        }else if (imc > 24.9){
+            document.getElementById('foto').src = "lib/img/ideal.png";
+
+        }else if (imc > 25 && imc < 29.9){
              result.innerHTML = imc;
               msg.innerHTML = "Acima do Peso";
-            document.getElementById('foto').src = "lib/img/gordo.png";
-        }
+            document.getElementById('foto').src = "lib/img/acima.png";
+
+        }else if (imc > 30 && imc < 34.9){
+             result.innerHTML = imc;
+              msg.innerHTML = "Obesidade Grau I";
+            document.getElementById('foto').src = "lib/img/acima.png";
+            
+        }else if (imc > 35 && imc < 39.9){
+             result.innerHTML = imc;
+              msg.innerHTML = "Obesidade Grau II";
+            document.getElementById('foto').src = "lib/img/acima.png";
+
+        }else if (imc >= 40){
+             result.innerHTML = imc;
+              msg.innerHTML = "Obesidade Grau III ou Mórbida";
+            document.getElementById('foto').src = "lib/img/acima.png";
+
     }
 
+}
